ce43c6962fc0a745541ee6cdf691d4898e52f4f6
THANHTUNG <ttungbmt@gmail.com>
2016-10-30 13:15:55 +0700

    Update Android

--- app/src/main/java/com/rio/ttungbmt/helloworld/MainActivity.java
+++ app/src/main/java/com/rio/ttungbmt/helloworld/MainActivity.java
@@ -2,14 +2,27 @@ package com.rio.ttungbmt.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+    Button btnClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnClick = (Button) findViewById(R.id.btnClick);
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello Trương Thanh Tùng", Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
